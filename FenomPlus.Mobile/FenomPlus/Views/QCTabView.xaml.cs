3364003f63c148c6ff71434d8c8c723827561b56
jamesrafferty <james.rafferty@caireinc.com>
2022-10-20 14:13:01 -0700

    Removed redundant xaml using styles.  Removed temporary code.  Tested.

---
+++ FenomPlus.Mobile/FenomPlus/Views/QCTabView.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace FenomPlus.Views
+{
+    public partial class QCTabView : ContentPage
+    {
+        public QCTabView()
+        {
+            InitializeComponent();
+
+            // Not necessary to set Binding Context to a ViewModel
+            // This page only handles UI interactions
+
+            Tab1Content.IsVisible = true;
+            Tab2Content.IsVisible = false;
+            Tab3Content.IsVisible = false;
+
+            UpdateTabButtonBorder();
+        }
+
+        public void Tab1ButtonClicked(object sender, EventArgs eventArgs)
+        {
+            Tab1Content.IsVisible = true;
+            Tab2Content.IsVisible = false;
+            Tab3Content.IsVisible = false;
+
+            UpdateTabButtonBorder();
+        }
+
+        public void Tab2ButtonClicked(object sender, EventArgs eventArgs)
+        {
+            Tab1Content.IsVisible = false;
+            Tab2Content.IsVisible = true;
+            Tab3Content.IsVisible = false;
+
+            UpdateTabButtonBorder();
+        }
+
+        public void Tab3ButtonClicked(object sender, EventArgs eventArgs)
+        {
+            Tab1Content.IsVisible = false;
+            Tab2Content.IsVisible = false;
+            Tab3Content.IsVisible = true;
+
+            UpdateTabButtonBorder();
+        }
+
+        private void UpdateTabButtonBorder()
+        {
+            Tab1ButtonBorder.IsVisible = Tab1Content.IsVisible;
+            Tab2ButtonBorder.IsVisible = Tab2Content.IsVisible;
+            Tab3ButtonBorder.IsVisible = Tab3Content.IsVisible;
+        }
+    }
+}
