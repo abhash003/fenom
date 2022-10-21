359dea9b34c6f64f2921b0c123d59f920f69de3e
Jason Peixoto <jason.peixoto@carieinc.com>
2022-10-21 18:08:06 -0400

    fixing issues with Android 12, need to add additional permissions

--- FenomPlus.Mobile/FenomPlus.Android/BLEPermission.cs
+++ FenomPlus.Mobile/FenomPlus.Android/BLEPermission.cs
@@ -8,8 +8,13 @@ namespace FenomPlus.Droid
         public override (string androidPermission, bool isRuntime)[] RequiredPermissions =>
             new List<(string androidPermission, bool isRuntime)>
         {
+            ("android.permission.BLUETOOTH", true),
+            ("android.permission.BLUETOOTH_ADMIN", true),
             ("android.permission.BLUETOOTH_SCAN", true),
-            ("android.permission.BLUETOOTH_CONNECT", true)
+            ("android.permission.BLUETOOTH_CONNECT", true),
+            ("android.permission.BLUETOOTH_ADVERTISE", true),
+            ("android.permission.ACCESS_COARSE_LOCATION", true),
+            ("android.permission.ACCESS_FINE_LOCATION", true),
         }.ToArray();
     }
 }
--- FenomPlus.Mobile/FenomPlus.Android/MainActivity.cs
+++ FenomPlus.Mobile/FenomPlus.Android/MainActivity.cs
@@ -9,6 +9,8 @@ using FenomPlus.Services;
 using FenomPlus.Droid.Services;
 using FenomPlus.Interfaces;
 using System.ComponentModel;
+using System.Linq;
+using Android;
 
 namespace FenomPlus.Droid
 {
@@ -26,13 +28,57 @@ namespace FenomPlus.Droid
             AppServices.Container.Register<INavigationService, NavigationService>().AsSingleton();
 
             LoadApplication(new App());
+            //CheckPermissions();
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="requestCode"></param>
+        /// <param name="permissions"></param>
+        /// <param name="grantResults"></param>
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
             Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
 
+            var permissionsList = permissions.ToList();
+            if (permissionsList.Any(p => p.Contains("BLUETOOTH")))
+            {
+                var permission = Android.OS.Build.VERSION.SdkInt >= ((Android.OS.BuildVersionCodes)0x1F) ? "android.permission.BLUETOOTH_SCAN" : "android.permission.BLUETOOTH";
+
+                var blePermission = permissionsList.FirstOrDefault(p => p == permission);
+                
+            }
             base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
-        }   
+        }
+
+        private void CheckPermissions()
+        {
+            string[] Permissions =
+            {
+                Manifest.Permission.Bluetooth,
+                Manifest.Permission.BluetoothAdmin,
+                "android.permission.BLUETOOTH_SCAN",
+                "android.permission.BLUETOOTH_CONNECT",
+                "android.permission.BLUETOOTH_ADVERTISE",
+                Manifest.Permission.AccessCoarseLocation,
+                Manifest.Permission.AccessFineLocation
+            };
+
+            bool minimumPermissionsGranted = true;
+
+            foreach (string permission in Permissions)
+            {
+                if (CheckSelfPermission(permission) != Permission.Granted)
+                {
+                    minimumPermissionsGranted = false;
+                }
+            }
+
+            if (!minimumPermissionsGranted)
+            {
+                RequestPermissions(Permissions, 0);
+            }
+        }
     }
 }
