17b2c0272007e3cbc0b6129eed95938f0b6edc16
Jason Peixoto <jason.peixoto@carieinc.com>
2022-10-24 14:13:55 -0400

    add navigation to each screen from services

--- FenomPlus.Mobile/FenomPlus.Android/Services/NavigationService.cs
+++ FenomPlus.Mobile/FenomPlus.Android/Services/NavigationService.cs
@@ -1,28 +1,142 @@
 ﻿using System;
-using Android.Content;
+using System.Threading.Tasks;
 using FenomPlus.Interfaces;
-using FenomPlus.Services;
 using Xamarin.Forms;
-using static Xamarin.Essentials.Platform;
 using Intent = Android.Content.Intent;
 
-namespace FenomPlus.Droid.Services
+namespace FenomPlus.Services
 {
-    public class NavigationService : BaseService, INavigationService
+    public partial class NavigationService : BaseService, INavigationService
     {
         public NavigationService(IAppServices services) : base(services)
         {
         }
 
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <exception cref="NotImplementedException"></exception>
         [Obsolete]
-        public void GotoBluetoothSettings()
+        public async Task GotoBluetoothSettings()
         {
             Intent bluetoothPicker = new Intent("android.bluetooth.devicepicker.action.LAUNCH");
             Forms.Context.StartActivity(bluetoothPicker);
+        }
+
+        public async Task BreathManeuverFeedbackView()
+        {
+            await Shell.Current.GoToAsync(new ShellNavigationState($"///{nameof(BreathManeuverFeedbackView)}"), false);
+        }
+
+        public async Task ChooseTestView()
+        {
+            await Shell.Current.GoToAsync(new ShellNavigationState($"///{nameof(ChooseTestView)}"), false);
+        }
+
+        public async Task DevicePowerOnView()
+        {
+            await Shell.Current.GoToAsync(new ShellNavigationState($"///{nameof(DevicePowerOnView)}"), false);
+        }
+
+        public async Task DeviceReadyView()
+        {
+            await Shell.Current.GoToAsync(new ShellNavigationState($"///{nameof(DeviceReadyView)}"), false);
+        }
+
+        public async Task HumanControlDisqualifiedView()
+        {
+            await Shell.Current.GoToAsync(new ShellNavigationState($"///{nameof(HumanControlDisqualifiedView)}"), false);
+        }
+
+        public async Task HumanControlPassedView()
+        {
+            await Shell.Current.GoToAsync(new ShellNavigationState($"///{nameof(HumanControlPassedView)}"), false);
+        }
+
+        public async Task HumanControlPreparingView()
+        {
+            await Shell.Current.GoToAsync(new ShellNavigationState($"///{nameof(HumanControlPreparingView)}"), false);
+        }
+
+        public async Task HumanControlPerformingView()
+        {
+            await Shell.Current.GoToAsync(new ShellNavigationState($"///{nameof(HumanControlPerformingView)}"), false);
+        }
+
+        public async Task NegativeControlFailView()
+        {
+            await Shell.Current.GoToAsync(new ShellNavigationState($"///{nameof(NegativeControlFailView)}"), false);
+        }
+
+        public async Task NegativeControlPassView()
+        {
+            await Shell.Current.GoToAsync(new ShellNavigationState($"///{nameof(NegativeControlPassView)}"), false);
+        }
+
+        public async Task NegativeControlPerformView()
+        {
+            await Shell.Current.GoToAsync(new ShellNavigationState($"///{nameof(NegativeControlPerformView)}"), false);
+        }
+
+        public async Task PairingView()
+        {
+            await Shell.Current.GoToAsync(new ShellNavigationState($"///{nameof(PairingView)}"), false);
+        }
+
+        public async Task PreparingStandardTestResultView()
+        {
+            await Shell.Current.GoToAsync(new ShellNavigationState($"///{nameof(PreparingStandardTestResultView)}"), false);
+        }
+
+        public async Task QualityControlView()
+        {
+            await Shell.Current.GoToAsync(new ShellNavigationState($"///{nameof(QualityControlView)}"), false);
+        }
+
+        public async Task QualityControlUsersView()
+        {
+            await Shell.Current.GoToAsync(new ShellNavigationState($"///{nameof(QualityControlUsersView)}"), false);
+        }
+
+        public async Task QualityControlDevicesView()
+        {
+            await Shell.Current.GoToAsync(new ShellNavigationState($"///{nameof(QualityControlDevicesView)}"), false);
+        }
+
+        public async Task StatusDeviceInfoView()
+        {
+            await Shell.Current.GoToAsync(new ShellNavigationState($"///{nameof(StatusDeviceInfoView)}"), false);
+        }
+
+        public async Task StopExhalingView()
+        {
+            await Shell.Current.GoToAsync(new ShellNavigationState($"///{nameof(StopExhalingView)}"), false);
+        }
+
+        public async Task TestErrorView()
+        {
+            await Shell.Current.GoToAsync(new ShellNavigationState($"///{nameof(TestErrorView)}"), false);
+        }
+
+        public async Task TestFailedView()
+        {
+            await Shell.Current.GoToAsync(new ShellNavigationState($"///{nameof(TestFailedView)}"), false);
+        }
+
+        public async Task TestResultsView()
+        {
+            await Shell.Current.GoToAsync(new ShellNavigationState($"///{nameof(TestResultsView)}"), false);
+        }
+
+        public async Task TutorialView()
+        {
+            await Shell.Current.GoToAsync(new ShellNavigationState($"///{nameof(TutorialView)}"), false);
+        }
+
+        public async Task ViewPastResultsView()
+        {
+            await Shell.Current.GoToAsync(new ShellNavigationState($"///{nameof(ViewPastResultsView)}"), false);
+        }
+
+        public async Task ViewRecentErrorsView()
+        {
+            await Shell.Current.GoToAsync(new ShellNavigationState($"///{nameof(ViewRecentErrorsView)}"), false);
         }
     }
 }
--- FenomPlus.Mobile/FenomPlus/Interfaces/INavigationService.cs
+++ FenomPlus.Mobile/FenomPlus/Interfaces/INavigationService.cs
@@ -1,10 +1,29 @@
 ﻿using System;
+using System.Threading.Tasks;
 
 namespace FenomPlus.Interfaces
 {
     public interface INavigationService
     {
-        void GotoBluetoothSettings();
+        Task GotoBluetoothSettings();
+        Task BreathManeuverFeedbackView();
+        Task ChooseTestView();
+        Task DevicePowerOnView();
+        Task DeviceReadyView();
+        Task HumanControlDisqualifiedView();
+        Task HumanControlPassedView();
+        Task HumanControlPreparingView();
+        Task HumanControlPerformingView();
+        Task NegativeControlFailView();
+        Task NegativeControlPassView();
+        Task NegativeControlPerformView();
+        Task PreparingStandardTestResultView();
+        Task QualityControlView();
+        Task StopExhalingView();
+        Task TestErrorView();
+        Task TestFailedView();
+        Task TestResultsView();
+        Task TutorialView();
     }
 }
 
