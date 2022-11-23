1497060d3c702ee6cbb395c367b04d77254c6c44
jamesrafferty <james.rafferty@caireinc.com>
2022-11-23 14:24:43 -0800

    Fixed a test results bug.

---
+++ FenomPlus.SDK/FenomPlus.SDK.Core/TestResultsView.xaml.cs
@@ -0,0 +1,34 @@
+﻿using FenomPlus.ViewModels;
+
+namespace FenomPlus.Views
+{
+    public partial class TestResultsView : BaseContentPage
+    {
+        private readonly TestResultsViewModel TestResultsViewModel;
+
+        public TestResultsView()
+        {
+            InitializeComponent();
+            BindingContext = TestResultsViewModel = new TestResultsViewModel();
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            TestResultsViewModel.OnAppearing();
+        }
+
+        protected override void OnDisappearing()
+        {
+            base.OnDisappearing();
+            TestResultsViewModel.OnDisappearing();
+        }
+
+        public override void NewGlobalData()
+        {
+            base.NewGlobalData();
+            TestResultsViewModel.NewGlobalData();
+        }
+    }
+}
+
