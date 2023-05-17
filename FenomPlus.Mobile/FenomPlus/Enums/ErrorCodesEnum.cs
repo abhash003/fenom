2fc958bb6e849c34056267924948d67eabe6f9bb
Uitak <ngcaire@gmail.com>
2023-05-16 20:41:03 -0700

    F150-480: fix a wording issue

--- FenomPlus.Mobile/FenomPlus/Enums/ErrorCodesEnum.cs
+++ FenomPlus.Mobile/FenomPlus/Enums/ErrorCodesEnum.cs
@@ -157,7 +157,7 @@ namespace FenomPlus.Enums
                     { 0x7E,  "undecl: 0x7E" },
                     { 0x7F,  "undecl: 0x7F" },
                     // section 8
-                    { 0x81,  "QC Negative Control Test failed, disable QC, then enable QC will rescue" },
+                    { 0x81,  "QC Negative Control Test failed, disable QC, then enable QC to reset" },
             };
 
             public ErrorCodeLookup()
