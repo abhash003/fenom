e1d9a88f3369ba29ba84b751c20a0896c8b2f73e
James Carter <cartersweng@gmail.com>
2023-07-07 17:09:40 -0700

    F150-712: increased maxdepth to 100,000 to test theory

--- FenomPlus.Mobile/FenomPlus/ViewModels/QualityControl/QualityControlViewModel.cs
+++ FenomPlus.Mobile/FenomPlus/ViewModels/QualityControl/QualityControlViewModel.cs
@@ -162,7 +162,7 @@ namespace FenomPlus.ViewModels
         public QualityControlViewModel()
         {
             // When de/serializing objects we reached the maximum default depth, so needed to increase it.
-            BsonMapper.Global.MaxDepth = 100;
+            BsonMapper.Global.MaxDepth = 100000;
 
             var localFolder = Environment.GetFolderPath(Environment.SpecialFolder.Personal);
             QCDatabasePath = Path.Combine(localFolder, @"QCDatabase.db");
